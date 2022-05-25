36b6f05bd6297fdd9e311c276dcef5cf16504ba4
G184852021 <218485@st.takushoku-u.ac.jp>
2022-05-25 22:12:06 +0900

    更新

--- kazuate.js
+++ kazuate.js
@@ -3,32 +3,37 @@
 let kotae = Math.floor(Math.random()*10) + 1;
 console.log('答え: ' + kotae);      // デバッグ用
 
-// 入力回数（予想回数）
 let kaisu = 0;
 let yoso;
-
-// 予想を4回実行する
-// 将来: ボタンを押したら， hantei() を呼び出すように修正する
+let seikai = 0;
 hantei();
-
-// ボタンを押した後の処理をする関数 hantei() の定義
 function hantei() {
-    yoso = 4;       // 第5回課題:テキストボックスの数値をここに代入
-    // 課題3-1：ここの判定処理を作成する．
-    //        ページに表示する方法はまだ習っていないので
-    //        判定結果はコンソールに出力すること
+    yoso = 4;
     kaisu = kaisu + 1;
+    let s = document.querySelector('span#kaisu');
+    s.textContent = kaisu;
+    let t = document.querySelector('span#yoso');
+    t.textContent = yoso;
     if (kaisu<4) {
         console.log(kaisu + "回目の予想:" + yoso);
         if (kaisu===3 && yoso !== kotae) {
-            let kekka = document.querySelector("まちがい．残念でした答えは" + kotae + "です．");
-            kekka.textContent = kekka;
+            let kotae="まちがい、残念でした答えは'+'kotae'+'です。";
+            let d=document.querySelector('p#result');
+            d.textContent=kotae;
         }else if (yoso>kotae) {
-            console.log("まちがい．答えはもっと小さいですよ");
+            let kotae="まちがい、答えはもっと小さいですよ。";
+            let d=document.querySelector('p#result');
+            d.textContent=kotae;
         }else if(yoso<kotae) {
-            console.log("まちがい．答えはもっと大きいですよ")
-        }else{
-            console.log("正解です．おめでとう!");
+            let kotae="まちがい、答えはもっと大きいですよ。";
+            let d=document.querySelector('p#result');
+            d.textContent=kotae;
+        }else if(yoso===kotae&&seikai<1){
+            let kotae="正解です。おめでとう！";
+            let d=document.querySelector('p#result');
+            d.textContent=kotae;
+            seikai++;
+            kaisu+=10;
         }
     }else{
         console.log("答えは" + kotae + "でした．すでにゲームは終わっています")
@@ -36,10 +41,3 @@ function hantei() {
 
 
 }
-
-let s = document.querySelector('span#kaisu');
-s.textContent = kaisu;
-let t = document.querySelector('span#yoso');
-t.textContent = yoso;
-let k = document.querySelector('span#kotae');
-k.textContent = kotae;
