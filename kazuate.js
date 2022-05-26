e16df5f73b9f6797578df861b84c6b25b752ef37
G184852021 <218485@st.takushoku-u.ac.jp>
2022-05-26 14:11:18 +0900

    更新

--- kazuate.js
+++ kazuate.js
@@ -3,13 +3,18 @@
 let kotae = Math.floor(Math.random()*10) + 1;
 console.log('答え: ' + kotae);      // デバッグ用
 
-let kaisu = 0;
+let kaisu = 1;
 let yoso;
 let seikai = 0;
-hantei();
+
+let b = document.querySelector('#print');
+b.addEventListener('click', hantei);
+
 function hantei() {
-    yoso = 4;
-    kaisu = kaisu + 1;
+    let i = document.querySelector('input[name="kaitou"]');
+    let r = i.value; 
+    r  = Number(r)
+    yoso = r;
     let s = document.querySelector('span#kaisu');
     s.textContent = kaisu;
     let t = document.querySelector('span#yoso');
@@ -17,27 +22,31 @@ function hantei() {
     if (kaisu<4) {
         console.log(kaisu + "回目の予想:" + yoso);
         if (kaisu===3 && yoso !== kotae) {
-            let kotae="まちがい、残念でした答えは'+'kotae'+'です。";
+            let k=("まちがい、残念でした答えは"+kotae+"です。");
             let d=document.querySelector('p#result');
-            d.textContent=kotae;
+            d.textContent=k;
         }else if (yoso>kotae) {
-            let kotae="まちがい、答えはもっと小さいですよ。";
+            let k="まちがい、答えはもっと小さいですよ。";
             let d=document.querySelector('p#result');
-            d.textContent=kotae;
+            d.textContent=k;
         }else if(yoso<kotae) {
-            let kotae="まちがい、答えはもっと大きいですよ。";
+            let k="まちがい、答えはもっと大きいですよ。";
             let d=document.querySelector('p#result');
-            d.textContent=kotae;
+            d.textContent=k;
         }else if(yoso===kotae&&seikai<1){
-            let kotae="正解です。おめでとう！";
+            let k="正解です。おめでとう！";
             let d=document.querySelector('p#result');
-            d.textContent=kotae;
+            d.textContent=k;
             seikai++;
-            kaisu+=10;
+            kaisu+=3;
         }
     }else{
-        console.log("答えは" + kotae + "でした．すでにゲームは終わっています")
+        let k="答えは" + kotae + "でした．すでにゲームは終わっています";
+        let d=document.querySelector('p#result');
+        d.textContent=k;
     }
+
+    kaisu = kaisu + 1;
 
 
 }
